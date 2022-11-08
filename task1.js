0999bfa0a41d8c0a974671608432db4727a97f50
Oliver Loftus <oliverloftus1@gmail.com>
2022-11-08 12:17:12 +0000

    capitalisedCats task 1

--- task1.js
+++ task1.js
@@ -1,3 +1,7 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map(function (cat){
+    const capitalCats = cat.toUpperCase();
+    console.log(capitalCats)
+    return capitalCats;
+});
